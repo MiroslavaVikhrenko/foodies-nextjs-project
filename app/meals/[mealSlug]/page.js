5f3435ad09b32dfd9b731fea171f7683ea7c141e
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-05-28 18:20:19 -0600

    Throwing not found errors for individual meals

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,15 @@
 import { getMeal } from '@/lib/meals';
 import classes from './page.module.css';
 import Image from 'next/image';
+import {notFound} from 'next/navigation';
 
 export default function MealDetailsPage({params}){
     const meal = getMeal(params.mealSlug);
+
+    // handle situation when meal does not exist
+    if (!meal) {
+        notFound();
+    }
 
     // override meal.instructions to fix instructions formatting (regex)
     meal.instructions = meal.instructions.replace(/\n/g, '<br />');
