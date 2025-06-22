0ec44b8687573ecdef37b4ce57e042f06b89fc49
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-06-22 17:00:49 -0600

    Add server-side input validation

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+'use client'; // to ensure it fetches any errors including errors on the client side
+
+export default function Error({error}) {
+    return (
+        <main className="error">
+            <h1>An error occurred!</h1>
+            <p>Failed to create meal.</p>
+        </main>
+    )
+}
