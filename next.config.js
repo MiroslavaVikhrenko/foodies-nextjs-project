f05fcfe71df7e934ddcc9150919ee157916eedce
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-06-22 18:48:46 -0600

    Store uploaded images in the cloud (AWS S3)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+    remotePatterns: [ // allows this specific S3 URL as a valid source for images.
+      {
+        protocol: 'https',
+        hostname: 'mirvikh-nextjs-demo-users-image.s3.us-east-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig
